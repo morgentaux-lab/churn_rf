491807b477f7cdad27c5b079dc1869e3865a93e7
Gromit <sunhavv@gmail.com>
2025-10-02 23:16:04 +0900

    app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1kCwbrBSLhvcIA7ymMTEtLtvxxHdyPOOq
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pickle
 import pandas as pd
